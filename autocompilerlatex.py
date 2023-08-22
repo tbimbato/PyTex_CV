f43f5b279b6fb1846e54fb24ab6598ed8cb7a188
tbimbato <tommi.bimbato@icloud.com>
2023-08-22 21:33:09 +0200

    update

--- autocompilerlatex.py
+++ autocompilerlatex.py
@@ -1,1 +1,14 @@
 # Wrote this loope for complete the latex template easily
+
+
+with open('CVTEX_EMPTY.tex') as texhand:
+    texlist = texhand.readlines()
+li = [x.strip() for x in texlist]
+
+
+for lines in li:
+    print(lines)
+with open("texcompiler2.py", "w") as ttemplate:
+    for lines in li:
+        ttemplate.write("f.write(" + "\"" + lines + ")\"" + ")\n")
+        ttemplate.write("f.write(" +  lines + ")\"" + ")\n")
