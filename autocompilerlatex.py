b2703a588430666d7bf9c029001ed5ce81e05835
tbimbato <tommi.bimbato@icloud.com>
2023-08-24 22:14:48 +0200

    update

--- autocompilerlatex.py
+++ autocompilerlatex.py
@@ -5,10 +5,8 @@ with open('CVTEX_EMPTY.tex') as texhand:
     texlist = texhand.readlines()
 li = [x.strip() for x in texlist]
 
-
 for lines in li:
     print(lines)
-with open("texcompiler2.py", "w") as ttemplate:
+with open("texcompiler.py", "w") as ttemplate:
     for lines in li:
-        ttemplate.write("f.write(" + "\"" + lines + ")\"" + ")\n")
-        ttemplate.write("f.write(" +  lines + ")\"" + ")\n")
+        ttemplate.write(r"fhand.write(" + "\"" + lines  + r"\n" + ")" + "\n")
